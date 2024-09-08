2cff12063d8264bf383c20da84f389486955eab2
Victor Engmark <victor@engmark.name>
2024-09-09 09:57:46 +1200

    fix(shellcheck-py): Support 0.10

--- overrides/default.nix
+++ overrides/default.nix
@@ -4228,13 +4228,17 @@ in
           preConfigure = let
             fakeCommand = "type('FakeCommand', (Command,), {'initialize_options': lambda self: None, 'finalize_options': lambda self: None, 'run': lambda self: None})";
           in ''
+            # remove setup.cfg since it causes remote package access
+            rm -f setup.cfg
+
             substituteInPlace setup.py \
               --replace-warn "'fetch_binaries': fetch_binaries," "'fetch_binaries': ${fakeCommand}," \
               --replace-warn "'install_shellcheck': install_shellcheck," "'install_shellcheck': ${fakeCommand},"
           '';
 
           propagatedUserEnvPkgs =
-            (old.propagatedUserEnvPkgs or [])
+            old.propagatedUserEnvPkgs
+            or []
             ++ [
               pkgs.shellcheck
             ];
